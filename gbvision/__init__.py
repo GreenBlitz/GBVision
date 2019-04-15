28533f447cc9bea01f460174cc9c84a15763dd51
idoheinemann <idohaineman@gmail.com>
2019-04-16 01:26:47 +0300

    fml

--- gbvision/__init__.py
+++ gbvision/__init__.py
@@ -1,10 +1,10 @@
-from utils import *
-from constants import *
-from exceptions import *
-from finders import *
-from gui import *
-from net import *
-from models import *
-from tools import *
+from .utils import *
+from .constants import *
+from .exceptions import *
+from .finders import *
+from .gui import *
+from .net import *
+from .models import *
+from .tools import *
 
 cv_config()
