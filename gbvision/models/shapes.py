6d1574a0c512367a0fb2790e0fa945dcbbffb869
idoheinemann <idohaineman@gmail.com>
2019-04-18 23:17:12 +0300

    fixed a small bug

--- gbvision/models/shapes.py
+++ gbvision/models/shapes.py
@@ -26,7 +26,7 @@ def rect_collision(r1, r2):
     return not (r1[0] > r2[0] + r2[2] or
                 r1[0] + r1[2] < r1[0] or
                 r1[1] > r2[1] + r2[3] or
-                r1[1] + r1[3] > r2[1])
+                r1[1] + r1[3] < r2[1])
 
 
 def _filter_inner_rects(rects):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gbvision',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python Vision Library for object tracking in the 3D physical space',
     license='Apache License 2.0',
     packages=['gbvision',
@@ -18,7 +18,7 @@ setup(
     author_email='idohaineman@gmail.com',
     keywords=['computer vision', 'frc', 'first', 'image processing'],
     url='https://github.com/GreenBlitz/GBVision',
-    download_url='https://github.com/GreenBlitz/GBVision/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/GreenBlitz/GBVision/archive/v0.1.1.tar.gz',
     install_requires=[
         'numpy',
         'opencv-python',
