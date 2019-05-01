4e046eda8eb96d0f0b78f1091646cf3e0b992948
idoheinemann <idohaineman@gmail.com>
2019-05-01 03:25:44 +0300

    changes to gitignore and setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gbvision',
-    version='0.1.4',
+    version='0.2.0',
     description='A Python Vision Library for object tracking in the 3D physical space',
     license='Apache License 2.0',
     packages=['gbvision',
@@ -18,14 +18,14 @@ setup(
     author_email='idohaineman@gmail.com',
     keywords=['computer vision', 'frc', 'first', 'image processing'],
     url='https://github.com/GreenBlitz/GBVision',
-    download_url='https://github.com/GreenBlitz/GBVision/archive/v0.1.4.tar.gz',
+    download_url='https://github.com/GreenBlitz/GBVision/archive/v0.2.0-beta.tar.gz',
     install_requires=[
         'numpy',
         'opencv-python',
         'opencv-contrib-python',
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 
         'Intended Audience :: Developers',  # Define that your audience are developers
