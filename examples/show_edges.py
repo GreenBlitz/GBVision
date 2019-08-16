838a9f3ffa08d8fac8d5325a335a61e6dc59f523
Ido Heinemann <35168632+idoheinemann@users.noreply.github.com>
2019-08-16 11:54:01 -0700

    fixed example

--- examples/show_edges.py
+++ examples/show_edges.py
@@ -4,8 +4,8 @@ import gbvision as gbv
 def main():
     camera = gbv.AsyncUSBCamera(0, gbv.UNKNOWN_CAMERA)
     camera.set_frame_size(640, 480)
-    orig_window = gbv.CameraWindow(camera, 'original')
-    edges_window = gbv.CameraWindow(camera, 'edges', drawing_pipeline=gbv.edges + gbv.gray)
+    orig_window = gbv.CameraWindow('original', camera)
+    edges_window = gbv.CameraWindow('edges', camera, drawing_pipeline=gbv.edges + gbv.gray)
     orig_window.show_async()
     edges_window.show()
 
