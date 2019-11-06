b06c0c1b076ce1951f117c1c98107311f5d56f52
TheOnion313 <royn77777@gmail.com>
2019-11-06 19:55:00 +0200

    fixed continues_rect

--- gbvision/continuity/continues_circle.py
+++ gbvision/continuity/continues_circle.py
@@ -6,7 +6,8 @@ from gbvision.utils.tracker import Tracker
 
 
 class ContinuesCircle(ContinuesShape):
-    def __init__(self, circle: Circle, tracker: Tracker = None, max_area_ratio = 2.0, max_distance_ratio = 1.0):
+    def __init__(self, circle: Circle, tracker: Tracker = None, max_area_ratio = 2.0, max_distance_ratio = 0.1):
+        assert max_area_ratio > 1.0
         self._circle = circle
         self._tracker = tracker if tracker is not None else Tracker()
         self._max_area_ratio = max_area_ratio
@@ -20,12 +21,14 @@ class ContinuesCircle(ContinuesShape):
         return sPI**2 * shape[1]**2
 
     def _shape_square_distance(self, shape: Circle) -> Number:
-        return self._shape[0[0]] - shape[0[0]]) ** 2 + (self._shape[0[1]] - shape[0[1]]) ** 2
+        return (self._shape[0[0]] - shape[0[0]]) ** 2 + (self._shape[0[1]] - shape[0[1]]) ** 2
 
     @staticmethod
-    def _from_bounding_rect(bounding_rect: Rect):
-        pass
+    def _from_bounding_rect(bounding_rect: Rect) -> Circle:
+        circle = [bounding_rect[0] + bounding_rect[2]/2, bounding_rect[3]/2 + bounding_rect[1], bounding_rect[3]/2]
+        return circle
 
     @staticmethod
-    def _to_bounding_rect(shape) -> Rect:
-        pass
+    def _to_bounding_rect(shape: Circle) -> Rect:
+        rect = [shape[0] - shape[2], shape[1] - shape[2], 2 * shape[2], 2 * shape[2]]
+        return rect
--- gbvision/continuity/continues_rect.py
+++ gbvision/continuity/continues_rect.py
@@ -1,12 +1,12 @@
-from gbvision.models.shapes import rect_collision
+from gbvision.constants.types import *
+from gbvision.continuity.continues_shape import ContinuesShape
 from gbvision.utils.tracker import Tracker
-import math
 
 
 # TODO add docs
 
-class ContinuesRect:
-    def __init__(self, rect, tracker=None, max_area_ratio=2.0, max_distance_ratio=0.1):
+class ContinuesRect(ContinuesShape):
+    def __init__(self, rect: Rect, tracker=None, max_area_ratio=2.0, max_distance_ratio=0.1):
         assert max_area_ratio > 1.0
         self._rect = rect
         self._count = 0
@@ -14,37 +14,35 @@ class ContinuesRect:
         self.max_area_ratio = max_area_ratio
         self.max_distance_ratio = max_distance_ratio
 
+    def _shape_collision(self, shape) -> bool:
+        return not(self._shape[0] > shape[0] + shape[2] or
+                   shape[0] > self._shape[0] + self._shape[2] or
+                   self._shape[1] > shape[1] + shape[3] or
+                   shape[1] > self._shape[1] + self._shape[3])
+
+    @staticmethod
+    def _shape_area(rect: Rect) -> Number:
+        return rect[2] * rect[3]
+
+    @staticmethod
+    def _from_bounding_rect(bounding_rect: Rect):
+        return bounding_rect
+
+    @staticmethod
+    def _to_bounding_rect(rect: Rect) -> Rect:
+        return rect
+
     @staticmethod
     def __rect_area(rect):
         return rect[2] * rect[3]
 
-    def get(self):
-        return self._rect
+    @staticmethod
+    def _shape_square_distance(self, rect: Rect) -> Number:
+        return (self._get_center(rect)[0] - self._shape)**2 + (self._get_center(rect)[1] - self._shape[1])**2
 
-    def _is_legal(self, rect):
-        if rect_collision(self._rect, rect):
-            if 1.0 / self.max_area_ratio <= self.__rect_area(self._rect) / self.__rect_area(rect) <= self.max_area_ratio:
-                x1, y1, w1, h1 = self._rect
-                x2, y2, w2, h2 = rect
-                if (x1 - x2) ** 2 + (y1 - y2) ** 2 <= (min(w1, w2) ** 2 + min(h1, h2) ** 2) * self.max_distance_ratio:
-                    return True
-        return False
+    @staticmethod
+    def _get_center(rect: Rect) -> Point:
+        center_x = (rect[0] + rect[3])/2
+        center_y = (rect[1] + rect[4])/2
+        return [center_x, center_y]
 
-    def update(self, frame, rect):
-        if self._is_legal(rect):
-            self._rect = rect
-            self._tracker.init(frame, rect)
-            self._count = 0
-            return True
-        return False
-
-    def update_not_found(self, frame):
-        self._count += 1
-        rect = self._tracker.update(frame)
-        if self._is_legal(rect):
-            self._rect = rect
-
-    def is_alive(self, max_wait: int = None):
-        if max_wait is not None:
-            return self._count <= max_wait
-        return True
