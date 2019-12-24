086abd0c604d7e7b5c3013c89cbc3a8ca91fb4e8
idoheinemann <idohaineman@gmail.com>
2019-12-24 14:53:01 +0200

    fixed convex shape collision (for real this time) and new release (v0.5.4)

--- gbvision/models/shapes.py
+++ gbvision/models/shapes.py
@@ -1,31 +1,43 @@
-from typing import List
+from typing import List, Callable
 
-from gbvision.constants.types import RotatedRect, Polygon, Rect, Circle
+from gbvision.constants.types import RotatedRect, Polygon, Rect, Circle, Shape
 from gbvision.utils.pipeline import PipeLine
 import numpy as np
 import cv2
 
+class __InnerShapeFilter(PipeLine):
+    """
+    filters out all the shapes that are colliding with shapes with a lower (smaller) index
+    maps from List[Shape] to List[Shape], where the output list is the input list without the colliding shapes
+    usually used on a sorted list, to remove any shape that is inside another shape
+
+    """
+    def __init__(self, collision_func: Callable[[Shape, Shape], bool]):
+        def _filter(shapes: List[Shape]) -> List[Shape]:
+            filtered_shapes = []
+            for i, shape in enumerate(shapes):
+                shape_invalid = False
+                for j in range(i):
+                    shape_invalid = collision_func(shape, shapes[j])
+                    if shape_invalid:
+                        break
+                if not shape_invalid:
+                    filtered_shapes.append(shape)
+            return filtered_shapes
+        PipeLine.__init__(self, _filter)
 
 def circle_collision(circ1: Circle, circ2: Circle) -> bool:
+    """
+    detects if two circles are colliding
+    :param circ1: the first circle
+    :param circ2: the second circle
+    :return: True if circles are colliding, False otherwise
+    """
     center1, r1 = circ1
     center2, r2 = circ2
     return (center1[0] - center2[0]) ** 2 + (center1[1] - center2[1]) ** 2 < (r1 + r2) ** 2
 
-
-@PipeLine
-def filter_inner_circles(circles: List[Circle]) -> List[Circle]:
-    filtered_circles = []
-    for i, circle in enumerate(circles):
-        circle_invalid = False
-        for j in range(i):
-            circle_invalid = circle_collision(circle, circles[j])
-            if circle_invalid:
-                break
-        if not circle_invalid:
-            filtered_circles.append(circle)
-
-    return filtered_circles
-
+filter_inner_circles = __InnerShapeFilter(circle_collision)
 
 def rect_collision(r1: Rect, r2: Rect) -> bool:
     return not (r1[0] > r2[0] + r2[2] or
@@ -34,87 +46,53 @@ def rect_collision(r1: Rect, r2: Rect) -> bool:
                 r1[1] + r1[3] < r2[1])
 
 
-@PipeLine
-def filter_inner_rects(rects: List[Rect]) -> List[Rect]:
-    filtered_rects = []
-    for i, rect in enumerate(rects):
-        rect_invalid = False
-        for j in range(i):
-            rect_invalid = rect_collision(rect, rects[j])
-            if rect_invalid:
-                break
-        if not rect_invalid:
-            filtered_rects.append(rect)
-    return filtered_rects
+filter_inner_rects = __InnerShapeFilter(rect_collision)
 
 
 def convex_shape_collision(shape1: Polygon, shape2: Polygon) -> bool:
     """
     detects collision between two convex shapes
     Note: if you are uncertain if a shape is convex, use convex_hull on it, it will make it convex
-    
+
     :param shape1: the first shape, as a contour
     :param shape2: the second shape, as a contour
     :return:
     """
-    for shape in [shape1, shape2]:
-        for idx, edge1 in enumerate(shape):
-            edge2 = shape[idx % len(shape1)]
+    shape1 = shape1.reshape(shape1.size // 2, 2)
+    shape2 = shape2.reshape(shape1.size // 2, 2)
 
-            normal = (edge1 - edge2)[::-1].T
-            normal[0] = -normal[0]
+    projection_axises = []
 
-            min_1 = max_1 = None
-            for edge in shape1:
-                projected = edge.dot(normal)[0, 0]
-                if min_1 is None or projected < min_1:
-                    min_1 = projected
+    negation_array = np.array([-1.0, 1.0])
 
-                if max_1 is None or projected > max_1:
-                    max_1 = projected
+    for i in range(len(shape1)):
+        projection_axises.append(shape1[(i + 1) % len(shape1)] - shape1[i])
+    for i in range(len(shape2)):
+        projection_axises.append(shape2[(i + 1) % len(shape2)] - shape2[i])
 
-            min_2 = max_2 = None
-            for edge in shape2:
-                projected = edge.dot(normal)[0, 0]
-                if min_2 is None or projected < min_2:
-                    min_2 = projected
+    for i in range(len(projection_axises)):
+        projection_axises[i] = projection_axises[i][::-1] * negation_array
 
-                if max_2 is None or projected > max_2:
-                    max_2 = projected
+    for axis in projection_axises:
+        proj1 = []
+        for vertix in shape1:
+            proj1.append(vertix.dot(axis))
+        proj1 = min(proj1), max(proj1)
 
-            if max_1 < min_2 or max_2 < min_1:
-                return False
+        proj2 = []
+        for vertix in shape2:
+            proj2.append(vertix.dot(axis))
+        proj2 = min(proj2), max(proj2)
+
+        if proj1[0] > proj2[1] or proj2[0] > proj1[1]:
+            return False
 
     return True
 
 
-@PipeLine
-def filter_inner_convex_shapes(shapes: List[Polygon]) -> List[Polygon]:
-    filtered_shapes = []
-    for i, shape in enumerate(shapes):
-        shape_invalid = False
-        for j in range(i):
-            shape_invalid = convex_shape_collision(shape, shapes[j])
-            if shape_invalid:
-                break
-        if not shape_invalid:
-            filtered_shapes.append(shape)
-    return filtered_shapes
-
+filter_inner_convex_shapes = __InnerShapeFilter(convex_shape_collision)
 
 def rotated_rect_collision(rr1: RotatedRect, rr2: RotatedRect) -> bool:
     return convex_shape_collision(cv2.boxPoints(rr1), cv2.boxPoints(rr2))
 
-
-@PipeLine
-def filter_inner_rotated_rects(rotated_rects: List[RotatedRect]) -> List[RotatedRect]:
-    filtered_rotated_rects = []
-    for i, rotated_rect in enumerate(rotated_rects):
-        rotated_rect_invalid = False
-        for j in range(i):
-            rotated_rect_invalid = rotated_rect_collision(rotated_rect, rotated_rects[j])
-            if rotated_rect_invalid:
-                break
-        if not rotated_rect_invalid:
-            filtered_rotated_rects.append(rotated_rect)
-    return filtered_rotated_rects
+filter_inner_rotated_rects = __InnerShapeFilter(rotated_rect_collision)
