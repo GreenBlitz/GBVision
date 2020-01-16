74884af57c444ec47720a7a4fb32e7c31afad2fe
idoheinemann <idohaineman@gmail.com>
2020-01-16 13:06:32 +0200

    added option for finders shapes hooks

--- gbvision/finders/circle_finder.py
+++ gbvision/finders/circle_finder.py
@@ -17,16 +17,21 @@ class CircleFinder(ObjectFinder):
      the finder is searching, the object needs to be white and the rest if the image black (doesn't
      have to be perfect)
     :param contour_min_area: the minimal area of a contour, used for FilterContours, default is 0 (no area limit)
+    :param contours_process: a pipeline to run on the list of contours (optional)
+    :param circles_process: a pipeline to run on the list of circles (optional)
     """
 
-    def __init__(self, threshold_func: FilterFunction, game_object, area_scalar=1.0, contour_min_area=0):
+    def __init__(self, threshold_func: FilterFunction, game_object, area_scalar=1.0, contour_min_area=0,
+                 contours_process=EMPTY_PIPELINE, circles_process=EMPTY_PIPELINE):
         ObjectFinder.__init__(self, game_object, area_scalar=area_scalar)
         self._full_pipeline = (EMPTY_PIPELINE +
                                threshold_func +
                                find_contours +
                                FilterContours(min_area=contour_min_area) +
+                               contours_process +
                                contours_to_circles_sorted +
-                               filter_inner_circles)
+                               filter_inner_circles +
+                               circles_process)
 
     def find_shapes(self, frame: Frame) -> List[Circle]:
         return self._full_pipeline(frame)
