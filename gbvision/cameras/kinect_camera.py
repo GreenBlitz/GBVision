e93ff82109236c967ca4077403e8519123f75e2b
gbprogrammers <Greenblitz4590programmers@gmail.com>
2020-01-28 12:19:34 +0200

    added kinect frame options, need to integrate with camera

---
+++ gbvision/cameras/kinect_camera.py
@@ -0,0 +1,37 @@
+from . import camera
+import freenect
+import numpy as np
+
+
+class KinectCamera(camera):
+    def __init__(self, port):
+        self.port = port
+
+    @staticmethod
+    def pretty_depth(depth):
+        """
+        Converts depth into a 'nicer' format for display
+
+        :param depth: A numpy array with 2 bytes per pixel
+        
+        :return: A numpy array that has been processed with unspecified datatype
+        """
+        np.clip(depth, 0, 2**10-1, depth)
+        depth >>=2
+        depth=depth.astype(np.uint8)
+        return depth
+    
+    @staticmethod
+    def get_ir_video(self):
+        frame = freenect.sync_get_video(self.port, freenect.VIDEO_IR_10BIT)[0]
+        frame = pretty_depth(frame)
+        return frame
+    
+    @staticmethod
+    def get_depth_image(self):
+        frame = freenect.sync_get_video(self.port)[0]
+        frame = pretty_depth(frame)
+        return frame
+    
+
+
