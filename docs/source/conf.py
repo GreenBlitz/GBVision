48dda0e48654d5d351eec0111bef06ae5f8e5e5a
Ofek Kfir <ofek.kfir@gmail.com>
2020-02-04 10:27:30 +0200

    changed conf.py and updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'GreenBlitz'
 
 
 # -- General configuration ---------------------------------------------------
-
+master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
