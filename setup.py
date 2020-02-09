b48dafd3791d560fd7cf3ceaf817d43aea13e75e
idoheinemann <idohaineman@gmail.com>
2020-02-09 22:34:48 +0200

    removed opencv python from install requirements

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
     download_url='https://github.com/GreenBlitz/GBVision/archive/v0.7.2-beta.tar.gz',
     install_requires=[
         'numpy',
-        'opencv-python',
         'opencv-contrib-python',
     ],
     classifiers=[
