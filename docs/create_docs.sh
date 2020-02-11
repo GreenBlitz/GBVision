bdbb462272fafefa3ac55f5a710a89fe106913cc
idoheinemann <idohaineman@gmail.com>
2020-02-11 12:11:17 +0200

    auto docs test 3

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -2,5 +2,10 @@
 
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 cd "$PROJECT_ROOT"/docs || (mkdir "$PROJECT_ROOT"/docs/ && (cd "$PROJECT_ROOT"/docs || exit 1))
-sphinx-apidoc -o sources/ "$PROJECT_ROOT"/gbvision
-make clean html
+sphinx-apidoc -o sources/ "$PROJECT_ROOT"/gbvision || exit 1
+if [ -n "$(command -v make)" ]; then
+  make clean html || exit 1
+else
+  cmd "/C make.bat clean html" || exit 1
+fi
+exit 0
