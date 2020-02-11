d6ec64e13f28cf28369240cbea3815e8f8201958
idoheinemann <idohaineman@gmail.com>
2020-02-11 12:15:12 +0200

    added auto delete of rst files

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 cd "$PROJECT_ROOT"/docs || (mkdir "$PROJECT_ROOT"/docs/ && (cd "$PROJECT_ROOT"/docs || exit 1))
+rm source/*.rst || exit 1
 sphinx-apidoc -o source/ "$PROJECT_ROOT"/gbvision || exit 1
 if [ -n "$(command -v make)" ]; then
   make clean html || exit 1
