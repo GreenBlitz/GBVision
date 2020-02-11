8fef5a527c112a9ec40624f16902f8af90b8a368
Alexey <theemailreader300@gmail.com>
2020-02-11 13:46:10 +0200

    added index manually

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -3,6 +3,7 @@
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 cd "$PROJECT_ROOT"/docs || (mkdir "$PROJECT_ROOT"/docs/ && (cd "$PROJECT_ROOT"/docs || exit 1))
 rm source/*.rst
+cp "$PROJECT_ROOT"/docs/cache/index.rst "$PROJECT_ROOT"/docs/source/index.rst
 if [ -n "$(command -v py)" ]; then
   alias py=py
 else
