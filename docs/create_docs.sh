0255408821fd8c7ba3973bdd85caadf27f34b533
idoheinemann <idohaineman@gmail.com>
2020-02-11 11:55:48 +0200

    auto docs generate test

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -1,4 +1,6 @@
 #DONT REMOVE __INITS__ FROM SUBMODULES AS IT COULD BREAK DOCS.
 
-sphinx-apidoc -o sources/ ../gbvision
+PROJECT_ROOT="$(git rev-parse --show-toplevel)"
+cd "$PROJECT_ROOT"/docs || (mkdir "$PROJECT_ROOT"/docs/ && (cd "$PROJECT_ROOT"/docs || exit 1))
+sphinx-apidoc -o sources/ "$PROJECT_ROOT"/gbvision
 make clean html
