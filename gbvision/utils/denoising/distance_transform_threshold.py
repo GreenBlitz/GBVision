9c765cdbb8f203252c9f566c6586e5fb048ecdf7
idoheinemann <idohaineman@gmail.com>
2020-02-13 12:28:37 +0200

    resolved circular dependency in a bad way

--- gbvision/utils/denoising/distance_transform_threshold.py
+++ gbvision/utils/denoising/distance_transform_threshold.py
@@ -1,7 +1,5 @@
-from gbvision.models.basic_ops import normalized_distance_transform
 from gbvision.utils.thresholds.color_threshold import ColorThreshold
 from gbvision.utils.pipeline import PipeLine
-from gbvision.constants.types import Number
 
 
 class DistanceTransformThreshold(PipeLine):
@@ -10,8 +8,11 @@ class DistanceTransformThreshold(PipeLine):
     removes from the original frame all white pixels that are at most min_distance_ratio normalized distance from the
     nearest black frame
 
+    :param min_distance_ratio: the minimum ratio between the maximum distance of a pixel from a white pixel in the
+    frame and a certain pixel for it to be included in the threshold, between 0 and 1
     """
-    def __init__(self, min_distance_ratio: Number):
+    def __init__(self, min_distance_ratio: float):
         PipeLine.__init__(self)
+        from gbvision.models.basic_ops import normalized_distance_transform
         self.functions += (normalized_distance_transform + ColorThreshold([[min_distance_ratio, 1.0]],
                                                                           ColorThreshold.THRESH_TYPE_GRAY)).functions
