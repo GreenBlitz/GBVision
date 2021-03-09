e851b2988b12496b5939e4679a8d89f48a07dad8
TheOnion313 <royn77777@gmail.com>
2021-03-09 18:56:42 +0200

    improved unittest and fixed typing for main function to come, replaces use of os with glob to list test images, replaced result to be the calculated threshold's result image and not the values themselves. fixed some calculations of accuracy

--- examples/median_threshold_stdv.py
+++ examples/median_threshold_stdv.py
@@ -1,8 +1,11 @@
 import unittest
-from os import listdir
+from glob import glob
 from pickle import load
+from typing import Tuple, List
 
 from cv2 import imread
+import gbvision as gbv
+import numpy as np
 
 IMAGES_PATH = 'threshold_images/'
 THRESHOLDS_PATH = 'threshold_images/results/'
@@ -11,15 +14,21 @@ THRESHOLDS_PATH = 'threshold_images/results/'
 class TestAutomaticThreshold(unittest.TestCase):
 
 	def test_images(self):
-		images = [i for i in listdir('threshold_images') if not i.endswith('.png')]
+		images = glob(f"{IMAGES_PATH}*.png")
 		accuracies = []
 		for path in images:
 			image = imread(IMAGES_PATH + path)
 			image_result = imread(THRESHOLDS_PATH + path)
 			bbox = load(path.replace('.png', '.p'))
-			result = find_threshold(image, bbox)
 
-			print(f"{path} accuracy: {sum(result - image_result)}")
+			thresh = find_threshold(image, bbox)
+			thresh_func = gbv.ColorThreshold(thresh, 'HSV')
+			result = thresh_func(image)
+
+			accuracy = sum(abs(result - image_result)) / (len(result) * len(result[0]))
+			print(f"{path} accuracy: {1 - accuracy}")
+			assert accuracy < 0.1
+			print("test passed\n")
 			accuracies.append(sum(abs(result - image_result)))
 
 		avg_accuracy = sum(accuracies) / len(accuracies)
@@ -27,5 +36,5 @@ class TestAutomaticThreshold(unittest.TestCase):
 		assert avg_accuracy < 0.05
 
 
-def find_threshold(image, bbox):
+def find_threshold(image: np.ndarray, bbox: np.ndarray) -> List[Tuple[int, int] * 3]:
 	pass
