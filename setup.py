9b432bcdc5bec831ff8bbb287d31da8ddd74da0e
idoheinemann <idohaineman@gmail.com>
2022-10-09 15:46:32 +0300

    new release - 0.7.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gbvision',
-    version='0.7.6',
+    version='0.7.7',
     description='A Python Vision Library for object tracking in the 3D physical space',
     license='Apache License 2.0',
     packages=['gbvision',
@@ -24,7 +24,7 @@ setup(
     author_email='idohaineman@gmail.com',
     keywords=['computer vision', 'frc', 'first', 'image processing'],
     url='https://github.com/GreenBlitz/GBVision',
-    download_url='https://github.com/GreenBlitz/GBVision/archive/v0.7.6-beta.tar.gz',
+    download_url='https://github.com/GreenBlitz/GBVision/archive/v0.7.7-beta.tar.gz',
     install_requires=[
         'numpy',
         'opencv-python',
@@ -39,5 +39,6 @@ setup(
         'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
