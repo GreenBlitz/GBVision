fbda62fa6bdd24dc72d2dbe73a86617830d1ea92
idoheinemann <idohaineman@gmail.com>
2022-10-12 13:11:59 +0300

    added async fragmented udp receiver

---
+++ gbvision/utils/net/async_fragmented_udp_stream_receiver.py
@@ -0,0 +1,12 @@
+from .async_stream_receiver import AsyncStreamReceiver
+from .fragmented_udp_stream_receiver import FragmentedUDPStreamReceiver
+
+
+class AsyncFragmentedUDPStreamReceiver(AsyncStreamReceiver, FragmentedUDPStreamReceiver):
+
+    def __init__(self, port, *args, **kwargs):
+        FragmentedUDPStreamReceiver.__init__(self, port, *args, **kwargs)
+        AsyncStreamReceiver.__init__(self, *args, **kwargs)
+
+    def _read(self):
+        return FragmentedUDPStreamReceiver.read(self)
